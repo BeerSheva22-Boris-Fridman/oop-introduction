8409350f8209ff6e519b87c0d07b8e512960b175
Kulicod <fridman1986@gmail.com>
2022-12-19 15:51:47 +0200

    CW 19/12

--- src/telran/memory/MemoryOperations.java
+++ src/telran/memory/MemoryOperations.java
@@ -1,28 +1,25 @@
 package telran.memory;
 
 public class MemoryOperations {
-	public static int getMaxAvailableMemory() {
-		boolean isWorked = false;
-		byte[] ar = null;
-		int left = 0;
-		int right = Integer.MAX_VALUE;
-		int middle = right / 2;
-		while (left <= right) {
-			isWorked = true;
-			ar = null;
-			try {
-				ar = new byte[middle];
-			} catch(Throwable e) {
-				isWorked = false;
-			}
-			if (isWorked) {
-				left = middle + 1;
-			}
-			else {
-				right = middle - 1;
-			}
-			middle = (left + right) / 2;
+public static int getMaxAvaibleMemory() {
+	
+	int minMemory = 0;
+	int maxMemory = Integer.MAX_VALUE;
+	int middle = maxMemory / 2;
+	int maxAvailable = 0;
+	
+	while(minMemory <= maxMemory) {
+		try {
+			byte ar[] = new byte[middle];
+			maxAvailable = middle;
+			minMemory = middle + 1;
+			
+		} catch (Throwable e) {
+			maxMemory = middle - 1;
+			
 		}
-		return middle;
+		middle = minMemory + (maxMemory - minMemory) / 2;
 	}
+	return maxAvailable;
 }
+} 
--- src/telran/memory/MemoryOperationsTest.java
+++ src/telran/memory/MemoryOperationsTest.java
@@ -2,12 +2,15 @@ package telran.memory;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class MemoryOperationsTest {
-	byte[] ar;
+	private static final long MGB = 1024*1024;
+	byte ar[];
 	
 	@Test
+	@Disabled
 	void maxMemoryTestTest() {
 		int maxMemory = MemoryOperations.getMaxAvailableMemory();
 		ar = new byte[maxMemory];
@@ -19,5 +22,13 @@ public class MemoryOperationsTest {
 			flagException = true;
 		}
 		assertTrue(flagException);
+	}
+	@Test
+	void standardMemoryMethodth( ) {
+		Runtime runtime = Runtime.getRuntime();
+		System.out.printf("Maximal memory JVM may requare from OS: %d, "
+		+ "current total JVM memory:%d, current free JVM memory: %d ",
+		runtime.maxMemory() / MGB, runtime.totalMemory() / MGB, runtime.freeMemory() / MGB);
+		
 	}
 }
