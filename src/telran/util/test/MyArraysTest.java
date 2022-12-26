4fa8be2038d842a4ab8690bb00b3b0fd3cd8b00d
Kulicod <fridman1986@gmail.com>
2022-12-26 12:19:57 +0200

    hw

--- src/telran/util/test/MyArraysTest.java
+++ src/telran/util/test/MyArraysTest.java
@@ -13,36 +13,33 @@ import org.junit.jupiter.api.Test;
 import telran.util.MyArrays;
 
 class MyArraysTest {
-	Integer numbers[] = {13, 2, -8, 47, 100, 10, -7, 7};
-	String strings[] = {
-			"ab", "abm", "abmb", "abmbc"	
-		};
+	Integer numbers[] = { 13, 2, -8, 47, 100, 10, -7, 7 };
+	String strings[] = { "ab", "abm", "abmb", "abmbc" };
 	Comparator<Integer> evenOddComparator = this::evenOddCompare;
+
 	@Test
 	@Disabled
 	void sortTest() {
-		String[] strings = {"abcd", "lmn", "zz"};
-		String[] expected = {"zz", "lmn", "abcd"};
-		
-		
+		String[] strings = { "abcd", "lmn", "zz" };
+		String[] expected = { "zz", "lmn", "abcd" };
+
 		MyArrays.sort(strings, new StringLengthComparator());
 		assertArrayEquals(expected, strings);
-		
-		
+
 	}
+
 	@Test
-	
+
 	void evenOddTest() {
-		
-		Integer expected[] = {-8, 2, 10, 100, 47, 13, 7, -7};
+
+		Integer expected[] = { -8, 2, 10, 100, 47, 13, 7, -7 };
 		MyArrays.sort(numbers, evenOddComparator);
 		assertArrayEquals(expected, numbers);
 	}
+
 	@Test
 	void binarySearchTest() {
-		String strings[] = {
-			"ab", "abm", "abmb", "abmbc"	
-		};
+		String strings[] = { "ab", "abm", "abmb", "abmbc" };
 		Comparator<String> comp = new StringsComparator();
 		assertEquals(0, MyArrays.binarySearch(strings, "ab", comp));
 		assertEquals(2, MyArrays.binarySearch(strings, "abmb", comp));
@@ -51,27 +48,51 @@ class MyArraysTest {
 		assertEquals(-3, MyArrays.binarySearch(strings, "abma", comp));
 		assertEquals(-5, MyArrays.binarySearch(strings, "lmn", comp));
 	}
+
 	@Test
 	void filterTest() {
 		int dividor = 2;
 		String subStr = "m";
 		Predicate<Integer> predEven = t -> t % dividor == 0;
 		Predicate<String> predSubstr = s -> s.contains(subStr);
-		String expectedStr[] = {
-				 "abm", "abmb", "abmbc"	
-			};
-		Integer expectedNumbers[] ={2, -8, 100, 10};
+		String expectedStr[] = { "abm", "abmb", "abmbc" };
+		Integer expectedNumbers[] = { 2, -8, 100, 10 };
 		assertArrayEquals(expectedStr, MyArrays.filter(strings, predSubstr));
 		assertArrayEquals(expectedNumbers, MyArrays.filter(numbers, predEven));
-		
+
 	}
+
 	int evenOddCompare(Integer o1, Integer o2) {
-		int remainder =  Math.abs(o1) % 2;
-		int res = remainder - Math.abs(o2) %2;
+		int remainder = Math.abs(o1) % 2;
+		int res = remainder - Math.abs(o2) % 2;
 		if (res == 0) {
 			res = remainder != 0 ? Integer.compare(o2, o1) : Integer.compare(o1, o2);
 		}
 		return res;
 	}
 
+	@Test
+	void filterTest_Negate() {
+
+		String subStr = "m";
+		String[] strings = { "a", "b", "m", "ab", "am", "bm" };
+		Predicate<String> predSubstr = s -> s.contains(subStr);
+		String expectedStr[] = { "a", "b", "ab" };
+		assertArrayEquals(expectedStr, MyArrays.removeIf(strings, predSubstr));
+
+	}
+	@Test
+	void booleanContainsTest() {
+		String  subStr = "m";
+		String[] strings = { "a", "b", "m", "ab", "am", "bm" };
+		String[] empty = {""};
+		String[] withoutPattern = { "a","b","ab","am","bm"};
+		String[] withNull = { "a","b","ab","am","bm", null};
+		MyArrays.contains(strings, subStr);	
+		assertEquals(true, MyArrays.contains(strings, subStr));
+		assertEquals(false, MyArrays.contains(empty, subStr));
+		assertEquals(false, MyArrays.contains(withoutPattern, subStr));
+		assertEquals(false, MyArrays.contains(withoutPattern, null));
+		assertEquals(true, MyArrays.contains(withNull, null));
+	}
 }
